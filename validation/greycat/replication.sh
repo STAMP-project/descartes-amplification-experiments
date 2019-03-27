3e52416d16eeedf5cfe086f51c5d47c5aacd13f7
oscarlvp <oscarlvp@gmail.com>
2019-03-27 18:40:08 +0100

    Validation files for greycat

---
+++ validation/greycat/replication.sh
@@ -0,0 +1,10 @@
+git clone https://github.com/datathings/greycat
+cd greycat
+git checkout 5ced869407f7612517fc354b41516eb300d8a3f1
+cd greycat
+mvn eu.stamp-project:pitmp-maven-plugin:1.3.7-EXPERIMENTS:descartes -DoutputFormats=METHODS,JSON,CSV,XML -DtimestampedReports=false -DreportsDirectory=target/ -DexcludedTestClasses=greycatTest.internal.task.RobustnessTests
+mvn eu.stamp-project:dissector-monitor:stack-trace -Doutput="target/stack-traces.json" -DclassificationsOfInterest="pseudo-tested,partially-tested"
+
+export MAVEN_OPTS="-Xmx10G"
+mvn -X eu.stamp-project:reneri:1.0-EXPERIMENTS:observeMethods -DtestTimes=3 -DexcludedTests="greycatTest.internal.heap.HeapSuperTimeTreeChunkTest,greycatTest.internal.heap.HeapEStructArrayTest,greycatTest.internal.heap.HeapTimeTreeChunkTest"
+mvn -X eu.stamp-project:reneri:1.0-EXPERIMENTS:observeTests -DtestTimes=3
